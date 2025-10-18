641a7c17f46a75fa339613207cb8ba6d4d42ea6e
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-10-18 21:20:08 +0530

    Added User Metadata endpoints tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -75,3 +75,73 @@ describe("Authentication", () => {
     expect(response.status).toBe(403);
   });
 });
+
+// All the user information endpoints are authenticated endpoints
+describe("User Metadata Endpoints", () => {
+  let token = "";
+  let avatarId = "";
+
+  // Before running the tests, beforeAll is called just once to get a valid token
+  beforeAll(async () => {
+    const username = `testuser-${Math.random()}`;
+    const password = "password123";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+
+    const avatarResponse = await axios.get(
+      `${BACKEND_URL}/api/v1/admmin/avatar`
+    );
+
+    token = response.data.token;
+    avatarId = avatarResponse.data.avatarId;
+  });
+
+  test("User can't update their metadata with a wrong avatar id", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId: "nonexistent-avatar-id",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    expect(response.status).toBe(400);
+  });
+
+  test("User can update their metadata with a valid avatar id", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId: avatarId,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    expect(response.status).toBe(200);
+  });
+
+  test("User is not able to update their metadata if the auth Header is not present", async () => {
+    const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
+      avatarId: avatarId,
+    });
+
+    expect(response.status).toBe(403); // Unauthorised
+  });
+});
