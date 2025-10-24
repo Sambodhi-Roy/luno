dc6ba8be1efa4b8a91c33f43de7c4478694d61f5
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-10-25 00:56:17 +0530

    DB Schema added

--- core/apps/http/src/routes/v1/admin.ts
+++ core/apps/http/src/routes/v1/admin.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 export const adminRouter = Router();
 
+adminRouter.post("/element", (req, res) => {});
+
 adminRouter.put("/element/:elementId", (req, res) => {});
 
 adminRouter.post("/avatar", (req, res) => {});
--- core/apps/http/src/routes/v1/space.ts
+++ core/apps/http/src/routes/v1/space.ts
@@ -4,11 +4,11 @@ export const spaceRouter = Router();
 
 spaceRouter.post("/", (req, res) => {});
 
-spaceRouter.delete("/:spaceId", (req, res) => {});
-
 spaceRouter.get("/all", (req, res) => {});
 
 spaceRouter.get("/:spaceId", (req, res) => {});
+
+spaceRouter.delete("/:spaceId", (req, res) => {});
 
 spaceRouter.post("/element", (req, res) => {});
 
