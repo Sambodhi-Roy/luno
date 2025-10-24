309a1993f48ec63375234e22e26cff36afc74ec2
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-10-24 21:31:11 +0530

    Added skeletons of all backend routes

---
+++ core/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+adminRouter.put("/element/:elementId", (req, res) => {});
+
+adminRouter.post("/avatar", (req, res) => {});
+
+adminRouter.post("/map", (req, res) => {});
---
+++ core/apps/http/src/routes/v1/index.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { userRouter } from "./user.js";
+import { spaceRouter } from "./space.js";
+import { adminRouter } from "./admin.js";
+
+export const router = Router();
+
+router.post("/signup", (req, res) => {
+  res.json({
+    message: "Signup route",
+  });
+});
+
+router.post("/signin", (req, res) => {
+  res.json({
+    message: "Login route",
+  });
+});
+
+router.get("/elements", (req, res) => {});
+
+router.get("/avatars", (req, res) => {});
+
+router.use("/user", userRouter);
+router.use("/space", spaceRouter);
+router.use("/admin", adminRouter);
