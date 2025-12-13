99b2ffe94b36fa93f183ddb9864b70e6ae644ac0
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-12-14 00:08:11 +0530

    Feat: Added Admin Middleware

---
+++ core/apps/http/src/middleware/adminAuth.middleware.ts
@@ -0,0 +1,51 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken"
+import client from "@repo/db/client";
+
+const getJWTSecret = () => {
+    const secret = process.env.JWT_SECRET;
+    if(!secret){
+        throw new Error("Missing JWT_SECRET in environment variables")        
+    }
+
+    return secret
+}
+
+interface JwtPayload{
+    userId: string
+}
+
+export const authenticateAdmin = async(req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies?.token
+
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorised"
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token, getJWTSecret()) as JwtPayload
+
+        const user = await client.user.findUnique({
+            where: {id:decoded.userId},
+            select: {role: true}
+        })
+
+        if(!user || user.role !== "Admin")
+        {
+            return res.status(403).json({
+                message: "Forbidden"
+            })
+        }
+
+        req.user = {id: decoded.userId}
+        next()
+    }
+    catch(e)
+    {
+        return res.status(403).json({
+            message: "Forbidden"
+        })
+    }
+}
