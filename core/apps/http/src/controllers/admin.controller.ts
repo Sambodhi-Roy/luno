0dfc19b815be63215f0c1f19e8988e95e6bc357f
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-12-14 00:21:50 +0530

    Feat: Added updateElement Controller to admin Controller

--- core/apps/http/src/controllers/admin.controller.ts
+++ core/apps/http/src/controllers/admin.controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import client from "@repo/db/client";
-import { createElementSchema } from "../types/index.js";
+import { createElementSchema, updateElementSchema } from "../types/index.js";
 import { parse } from "dotenv";
 
 export const createElement = async(req:Request, res: Response) => {
@@ -27,6 +27,43 @@ export const createElement = async(req:Request, res: Response) => {
 
         return res.status(200).json({
             id: element.id
+        })
+    }
+    catch(e)
+    {
+        return res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
+
+export const updateElement = async(req: Request, res: Response)=>{
+    const {elementId} = req.params
+
+    if(!elementId){
+        return res.status(400).json({
+            message: "elementId required"
+        })
+    }
+
+    const parsed = updateElementSchema.safeParse(req.body)
+    if(!parsed.success){
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: parsed.error
+        })
+    }
+
+    try{
+        await client.element.update({
+            where: {id: elementId},
+            data:{
+                imageUrl: parsed.data.imageUrl
+            }
+        })
+
+        return res.status(200).json({
+            message: "Element updated"
         })
     }
     catch(e)
