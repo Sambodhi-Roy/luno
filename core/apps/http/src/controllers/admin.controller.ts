3de8172af2a40b79ad8f6aaee0cd63b90b265c14
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-12-14 00:16:32 +0530

    Feat: Added createElement Controller in Admin Controller

---
+++ core/apps/http/src/controllers/admin.controller.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from "express";
+import client from "@repo/db/client";
+import { createElementSchema } from "../types/index.js";
+import { parse } from "dotenv";
+
+export const createElement = async(req:Request, res: Response) => {
+    const parsed = createElementSchema.safeParse(req.body)
+
+    if(!parsed.success){
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: parsed.error
+        })
+    }
+
+    const {imageUrl, width, height, static: isStatic} = parsed.data
+
+    try{
+        const element = await client.element.create({
+            data:{
+                imageUrl,
+                width,
+                height,
+                static: isStatic
+            }
+        })
+
+        return res.status(200).json({
+            id: element.id
+        })
+    }
+    catch(e)
+    {
+        return res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
