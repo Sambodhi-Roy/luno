cac54518aed45aa16dc91dd3ad72eae35a5e1d4c
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2026-01-22 23:09:38 +0530

    Feat: Added Avatar in the Game Scene

---
+++ core/apps/web/game/entities/Player.ts
@@ -0,0 +1,72 @@
+import * as Phaser from "phaser"
+
+export type Direction = "up" | "down" | "left" | "right";
+
+export class Player{
+    sprite: Phaser.GameObjects.Sprite;
+    speed = 120;
+    direction: Direction = "down";
+
+    keys:{
+        up: Phaser.Input.Keyboard.Key;
+        down: Phaser.Input.Keyboard.Key;
+        left: Phaser.Input.Keyboard.Key;
+        right: Phaser.Input.Keyboard.Key; 
+    };
+
+    scene: Phaser.Scene;
+
+    constructor(scene: Phaser.Scene, x:number, y:number){
+        this.scene = scene
+
+        // Creating the sprite
+        this.sprite = scene.add.sprite(x,y,"adam");
+        this.sprite.setScale(2) //16px -> 32px
+
+        this.keys = scene.input.keyboard!.addKeys({
+            up: "W",
+            down: "S",
+            left: "A",
+            right: "D",
+        }) as any;
+
+        // Start with idle down
+        this.sprite.play("idle-down")
+    }
+
+    update(dt:number){
+        let vx = 0;
+        let vy = 0;
+
+        if(this.keys.left.isDown){
+            vx = -1;
+            this.direction = "left"
+        }else if(this.keys.right.isDown){
+            vx = 1
+            this.direction = "right"
+        }
+
+        if(this.keys.up.isDown){
+            vy = -1
+            this.direction = "up"
+        }else if (this.keys.down.isDown){
+            vy = 1
+            this.direction = "down"
+        }
+
+        // Normalise diagonal movement
+        if(vx!=0 || vy!=0){
+            const len = Math.sqrt(vx*vx + vy*vy)
+            vx /= len
+            vy /=len
+
+            this.sprite.x += vx * this.speed*dt
+            this.sprite.y += vy * this.speed*dt
+
+            this.sprite.play(`walk-${this.direction}`, true)
+        }
+        else{
+            this.sprite.play(`walk-${this.direction}`, true)
+        }
+    }
+}
--- core/apps/web/game/scenes/WorldScene.ts
+++ core/apps/web/game/scenes/WorldScene.ts
@@ -1,6 +1,9 @@
 import * as Phaser from "phaser";
+import { Player } from "../entities/Player";
 
 export class WorldScene extends Phaser.Scene {
+  player!: Player
+
   constructor() {
     super("WorldScene");
     console.log("WorldScene constructor");
@@ -11,42 +14,86 @@ export class WorldScene extends Phaser.Scene {
     this.load.tilemapTiledJSON("map", "/assets/maps/sample-map.tmj");
     this.load.image(
     "Room_Builder_free_32x32",
-    "/assets/tilesets/Room_Builder_free_32x32.png"
-  );
+    "/assets/tilesets/Room_Builder_free_32x32.png")
+    
+    this.load.image(
+      "Interiors_free_32x32",
+      "/assets/tilesets/Interiors_free_32x32.png"
+    );
 
-  this.load.image(
-    "Interiors_free_32x32",
-    "/assets/tilesets/Interiors_free_32x32.png"
-  );
+    this.load.spritesheet("adam","/assets/characters/adam.png",{
+      frameWidth: 16,
+      frameHeight: 32,
+    })
   }
 
   create() {
-  console.log("WorldScene create()");
+    console.log("WorldScene create()");
 
-  const map = this.make.tilemap({ key: "map" });
+    const map = this.make.tilemap({ key: "map" });
 
-  const roomBuilder = map.addTilesetImage("Sample2", "Room_Builder_free_32x32");
-  const interiors = map.addTilesetImage("Sample", "Interiors_free_32x32");
+    const roomBuilder = map.addTilesetImage("Sample2", "Room_Builder_free_32x32");
+    const interiors = map.addTilesetImage("Sample", "Interiors_free_32x32");
 
-  if (!roomBuilder || !interiors) {
-    throw new Error("Tilesets not found. Check tileset names in Tiled.");
+    if (!roomBuilder || !interiors) {
+      throw new Error("Tilesets not found. Check tileset names in Tiled.");
+    }
+
+    map.createLayer("Ground", [roomBuilder, interiors], 0, 0);
+    map.createLayer("Walls", [roomBuilder, interiors], 0, 0);
+    map.createLayer("Objects", [roomBuilder, interiors], 0, 0);
+
+    const collisionLayer = map.createLayer("Collision", [roomBuilder, interiors], 0, 0);
+    collisionLayer?.setVisible(false);
+
+    this.createAnimations();
+
+    this.player = new Player(this, map.widthInPixels/2, map.heightInPixels/2);
+
+    const cam = this.cameras.main;
+
+    cam.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    cam.centerOn(
+    map.widthInPixels / 2,
+    map.heightInPixels / 2
+    );
   }
 
-  map.createLayer("Ground", [roomBuilder, interiors], 0, 0);
-  map.createLayer("Walls", [roomBuilder, interiors], 0, 0);
-  map.createLayer("Objects", [roomBuilder, interiors], 0, 0);
+  update(_:number, delta: number){
+    const dt = delta/1000
+    this.player.update(dt)
+  }
 
-  const collisionLayer = map.createLayer("Collision", [roomBuilder, interiors], 0, 0);
-  collisionLayer?.setVisible(false);
+  private createAnimations(){
+    const anims = this.anims;
 
-  const cam = this.cameras.main;
+    anims.create({
+      key: "idle-right",
+      frames:[{key: "adam", frame:0}],
+      frameRate:1,
+      repeat: -1,
+    })
+    
+    anims.create({
+      key: "idle-up",
+      frames:[{key: "adam", frame:1}],
+      frameRate:1,
+      repeat: -1,
+    })
 
-cam.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    anims.create({
+      key: "idle-left",
+      frames:[{key: "adam", frame:2}],
+      frameRate:1,
+      repeat: -1,
+    })
 
-cam.centerOn(
-  map.widthInPixels / 2,
-  map.heightInPixels / 2
-);
-
-}
+    anims.create({
+      key: "idle-down",
+      frames:[{key: "adam", frame:3}],
+      frameRate:1,
+      repeat: -1,
+    })
+  }
 }
